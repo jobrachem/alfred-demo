1fb0afbd059f2983a4beb64404ab0d178f2c373f
Johannes Brachem <jobrachem@posteo.de>
2019-08-12 15:52:43 +0200

    welcome page, pages and sections

--- script.py
+++ script.py
@@ -2,9 +2,9 @@
 '''
 Experiment script using Alfred - A library for rapid experiment development.
 
-Experiment author: Johannes Brachem <jobrachem@posteo.de>
+Experiment author: Johannes Brachem <brachem@psych.uni-goettingen.de>
 
-Description: This is a basic template for an alfred experiment.
+Description: This is a showcase experiment, providing an overview of alfred's basic features.
 '''
 
 
@@ -25,9 +25,15 @@ from alfred import Experiment
 # - Section 2: Global variables and functions - #
 #################################################
 EXP_TYPE = "web"
-EXP_NAME = "template"
+EXP_NAME = "Alfred Demo"
 EXP_VERSION = "1.0"
-EXP_AUTHOR_MAIL = "your@email.com"
+EXP_AUTHOR_MAIL = "brachem@psych.uni-goettingen.de"
+
+
+# import the .xml file, containing text snippets
+texts = parse_xml_to_dict("files/text_snippets.xml")
+code = parse_xml_to_dict("files/code_snippets.xml", code=True)
+print(code)
 
 
 #################################
@@ -47,18 +53,38 @@ class Script(object):
 
         # --- START OF EDITABLE AREA --- #
 
-        # Define pages
-        p10 = WebCompositePage(title="Hello, world!")
+        # --- page 10 --- #
+        p10 = WebCompositePage(title="Welcome to Alfred!")
 
-        p10_text = TextElement("This is a basic template.")
-        p10.append(p10_text)
+        t10 = TextElement(texts["t10"])
+        c10 = CodeElement(code["c10"], lang="python")
+        t20 = TextElement(texts["t20"])
+        p10.append(t10, c10, t20)
 
-        # Initialize and fill sections
+        # --- page 20 --- #
+        p20 = WebCompositePage(title="Pages and Sections")
+
+        # elements to pages
+        t30 = TextElement(texts["t30"])
+        c20 = CodeElement(code["c20"], lang="python")
+
+        # pages to sections
+        t40 = TextElement(texts["t40"])
+        c30 = CodeElement(code["c30"], lang="python", first=False)
+
+        # sections to experiment
+        t50 = TextElement(texts["t50"])
+        c40 = CodeElement(code["c40"], lang="python", first=False)
+
+        # append elements to p20
+        p20.append(t30, c20, t40, c30, t50, c40)
+
+        # --- Initialize and fill sections --- #
         main = Section()
-        main.append(p10)
+        main.append(p10, p20)
 
         # Append sections and pages to experiment
-        exp.page_controller.append_item(main)
+        exp.page_controller.append(main)
 
         # --- END OF EDITABLE AREA --- #
 
